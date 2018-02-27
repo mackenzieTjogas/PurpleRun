74605b5005119751fb92489586c1aa0a3c591ecc
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2018-02-26 17:36:04 -0800

    Using Xcode's version control

--- 402Proj/ViewController.swift
+++ 402Proj/ViewController.swift
@@ -25,6 +25,24 @@ class ViewController: UIViewController {
     message: "You will only get 2 trys", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "Enter", style:
     UIAlertActionStyle.default, handler: nil))
+        // call the enter function below in the handler ^^
+        present(alertController, animated: true, completion: nil)
+        // Eventually, when 'Enter' is hit, a pin code/password input
+        // box needs to appear. Then when the password is inputted and the user
+        // clicks 'submit' they should be taken to the second page. Right now this is done
+        // by the 'ChangeLater' button on the top right of homepage. This button won't be
+        // needed once this is implemented correctly. 
+    }
+    
+    func enter() {
+        performSegue(withIdentifier: "testSegue", sender: self)
+    }
+    
+    @IBAction func showEmergencyMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "You will now need to enter your pin",
+    message: "This call cannot be stopped", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Call", style:
+            UIAlertActionStyle.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
 
