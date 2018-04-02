3b0a6436dacba2c13fc09ef4f6b77106cfb75f96
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2018-04-01 21:22:51 -0700

    removed initial enter button code

--- 402Proj/ViewController.swift
+++ 402Proj/ViewController.swift
@@ -25,20 +25,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "You will now need to enter your pin",
-    message: "You will only get 2 trys", preferredStyle: UIAlertControllerStyle.alert)
-        alertController.addAction(UIAlertAction(title: "Enter", style:
-    UIAlertActionStyle.default, handler: nil))
-        // call the enter function below in the handler ^^
-        present(alertController, animated: true, completion: nil)
-        // Eventually, when 'Enter' is hit, a pin code/password input
-        // box needs to appear. Then when the password is inputted and the user
-        // clicks 'submit' they should be taken to the second page. Right now this is done
-        // by the 'ChangeLater' button on the top right of homepage. This button won't be
-        // needed once this is implemented correctly. 
-    }
+
+    // removed the below button: 
+//    @IBAction func showMessage(sender: UIButton) {
+//        let alertController = UIAlertController(title: "You will now need to enter your pin",
+//    message: "You will only get 2 trys", preferredStyle: UIAlertControllerStyle.alert)
+//        alertController.addAction(UIAlertAction(title: "Enter", style:
+//    UIAlertActionStyle.default, handler: nil))
+//        // call the enter function below in the handler ^^
+//        present(alertController, animated: true, completion: nil)
+//        // Eventually, when 'Enter' is hit, a pin code/password input
+//        // box needs to appear. Then when the password is inputted and the user
+//        // clicks 'submit' they should be taken to the second page. Right now this is done
+//        // by the 'ChangeLater' button on the top right of homepage. This button won't be
+//        // needed once this is implemented correctly.
+//    }
     
     func enter() {
         performSegue(withIdentifier: "testSegue", sender: self)
